f47455f50b20c9836143a96862a9f757841ccbf5
Jerrico Gamis <jecklgamis@gmail.com>
2019-05-04 13:38:09 +0800

    Add ExampleGetSimuation, ExamplePostSimuation

---
+++ src/main/scala/gatling/test/example/simulation/ExamplePostSimulation.scala
@@ -0,0 +1,29 @@
+package gatling.test.example.simulation
+
+import gatling.test.example.simulation.PerfTestConfig.{baseUrl, durationMin, maxResponseTimeMs, meanResponseTimeMs}
+import io.gatling.core.Predef.{StringBody, constantUsersPerSec, global, scenario, _}
+import io.gatling.http.Predef.{http, status, _}
+
+import scala.concurrent.duration._
+
+class ExamplePostSimulation extends Simulation {
+  val httpConf = http.baseUrl(baseUrl)
+  val postUsers = scenario("Root end point calls")
+    .exec(http("root end point")
+      .post("")
+      .header("Content-Type", "application/json")
+      .header("Accept-Encoding", "gzip")
+      .body(StringBody("{}"))
+      .check(status.is(200))
+    )
+  setUp(postUsers.inject(
+    constantUsersPerSec(PerfTestConfig.requestPerSecond) during (durationMin minutes))
+    .protocols(httpConf))
+    .assertions(
+      global.responseTime.max.lt(meanResponseTimeMs),
+      global.responseTime.mean.lt(maxResponseTimeMs),
+      global.successfulRequests.percent.gt(95)
+    )
+}
+
+
